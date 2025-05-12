8347cf719c7391f6b5c08acb74091c8b69f23b75
Anu Kriti Wadhwa <anu.kriti.w@gmail.com>
2025-05-12 17:00:18 +0100

    Add debug code for vieweing density optimisation and debug outputs.

--- generate_grass_rib.py
+++ generate_grass_rib.py
@@ -2,6 +2,7 @@ import random
 import os
 import shutil
 import math
+# import colorsys
 
 # Parameters
 x_range = (-10, 10)
@@ -10,8 +11,8 @@ y_base = -1.0
 min_height = 0.08
 max_height = 0.15
 
-patch_size = 2.0        # each patch covers a 2x2 area
-min_blades_per_patch = 100 # density control per patch
+patch_size = 2.0            # each patch covers a 2x2 area
+min_blades_per_patch = 100
 max_blades_per_patch = 4000
 
 output_dir = 'grass_patches'
@@ -29,8 +30,8 @@ z_start, z_end = z_range
 max_patch_distance = math.sqrt(x_end**2 + z_end**2)
 fade_start = 0.2 * max_patch_distance  # full density until this distance
 fade_end = max_patch_distance          # taper to min beyond this
-print(fade_start)
-print(fade_end)
+# print(fade_start)
+# print(fade_end)
 
 num_patches_x = int((x_end - x_start) / patch_size)
 num_patches_z = int((z_end - z_start) / patch_size)
@@ -44,11 +45,26 @@ AttributeBegin
 \t\t"float specularRoughness" [0.3]
 """
 
+# TODO: delete debug; visualise patches
+# distinct_colors = [
+#     (1.0, 0.0, 0.0),   # red
+#     (0.0, 1.0, 0.0),   # green
+#     (0.0, 0.0, 1.0),   # blue
+#     (1.0, 1.0, 0.0),   # yellow
+#     (1.0, 0.0, 1.0),   # magenta
+#     (0.0, 1.0, 1.0),   # cyan
+#     (1.0, 0.5, 0.0),   # orange
+#     (0.5, 0.0, 1.0),   # purple
+#     (0.5, 1.0, 0.0),   # lime
+#     (0.0, 0.5, 1.0),   # sky blue
+# ]
+
 patch_paths = []
 
 # generate each patch
 for i in range(num_patches_x):
     for j in range(num_patches_z):
+        # r, g, b = colorsys.hsv_to_rgb(hue, 0.9, 0.9)es_z):
         x0 = x_start + i * patch_size
         x1 = x0 + patch_size
         z0 = z_start + j * patch_size
@@ -59,7 +75,7 @@ for i in range(num_patches_x):
         patch_center_z = (z0 + z1) / 2
         distance = math.sqrt(patch_center_x**2 + patch_center_z**2)
 
-        # determine blade count based on distance
+        # determine blade # TODO: delete debug; visualise patchescount based on distance
         if distance < fade_start:
             blades_per_patch = max_blades_per_patch
         elif distance >= fade_end:
@@ -69,11 +85,32 @@ for i in range(num_patches_x):
             blades_per_patch = int(
                 max_blades_per_patch * (1 - t) + min_blades_per_patch * t
             )
-        print(blades_per_patch)
+        # print(blades_per_patch)
 
         filename = os.path.join(output_dir, f"grass_patch_{i}_{j}.rib")
         patch_paths.append(filename)
-        print(filename)
+        # print(filename)
+
+        # assign unique HSV-based color for debug
+        # patch_index = i * num_patches_z + j
+
+        # TODO: delete debug; visualise density by distance
+        # t = (blades_per_patch - min_blades_per_patch) / (max_blades_per_patch - min_blades_per_patch)
+        # hue = (1.0 - t) * 0.4  # 0.4 = red, 0 = green (or flip for green → red)
+        # r, g, b = colorsys.hsv_to_rgb(hue, 1.0, 1.0)
+
+        # TODO: delete debug; visualise patches
+        # color_index = (i * num_patches_z + j) % len(distinct_colors)
+        # print(distinct_colors[color_index])
+        # r, g, b = distinct_colors[color_index]
+
+        # rib_header = f"""
+        #     AttributeBegin
+        #     \tBxdf "PxrSurface" "PxrGrass"
+        #     \t\t"color diffuseColor" [{r:.3f} {g:.3f} {b:.3f}]
+        #     \t\t"float diffuseGain" [1.0]
+        #     \t\t"float specularRoughness" [0.3]
+        #     """
 
         with open(filename, "w") as f:
             f.write(rib_header)
